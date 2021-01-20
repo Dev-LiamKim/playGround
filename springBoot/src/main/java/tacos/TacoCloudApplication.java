5102950f639065c50d35c92da2132fa4f63ed023
Dev-LiamKim <johnbhjbg@gmail.com>
2021-01-20 14:34:53 +0000

    first git push

---
+++ springBoot/src/main/java/tacos/TacoCloudApplication.java
@@ -0,0 +1,10 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SrpingBootApplication;
+@SrpingBootApplication
+public class TacoCloudApplication{
+    public static void main(String[] args){
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+}
---
+++ springBoot/src/test/java/tacos/TacoCloudApplicationTests.java
@@ -0,0 +1,11 @@
+package tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SrpingBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests{
+    @Test
+    void contextLoads(){
+    }   
+}
